13eb670700c6c081c6ccff54c92d045cb92ba564
MejstrikRudolf <mejstrik.skola@gmail.com>
2020-07-16 22:43:10 +0200

    first commit

---
+++ rectangle_svg_gen.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace recrangle_svg_gen
+{
+    partial class Rectangle_generator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Colors = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numeric_blue = new System.Windows.Forms.NumericUpDown();
+            this.numeric_green = new System.Windows.Forms.NumericUpDown();
+            this.numeric_red = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numeric_height = new System.Windows.Forms.NumericUpDown();
+            this.numeric_width = new System.Windows.Forms.NumericUpDown();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numeric_posy = new System.Windows.Forms.NumericUpDown();
+            this.numeric_posx = new System.Windows.Forms.NumericUpDown();
+            this.button_add = new System.Windows.Forms.Label();
+            this.button_end = new System.Windows.Forms.Label();
+            this.Colors.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_red)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_width)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_posy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_posx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Colors
+            // 
+            this.Colors.Controls.Add(this.label3);
+            this.Colors.Controls.Add(this.label2);
+            this.Colors.Controls.Add(this.label1);
+            this.Colors.Controls.Add(this.numeric_blue);
+            this.Colors.Controls.Add(this.numeric_green);
+            this.Colors.Controls.Add(this.numeric_red);
+            this.Colors.Location = new System.Drawing.Point(12, 12);
+            this.Colors.Name = "Colors";
+            this.Colors.Size = new System.Drawing.Size(200, 112);
+            this.Colors.TabIndex = 0;
+            this.Colors.TabStop = false;
+            this.Colors.Text = "Colors";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Blue";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Green";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Red";
+            // 
+            // numeric_blue
+            // 
+            this.numeric_blue.Location = new System.Drawing.Point(74, 79);
+            this.numeric_blue.Name = "numeric_blue";
+            this.numeric_blue.Size = new System.Drawing.Size(120, 23);
+            this.numeric_blue.TabIndex = 2;
+            numeric_blue.Maximum = 255;
+            // 
+            // numeric_green
+            // 
+            this.numeric_green.Location = new System.Drawing.Point(74, 50);
+            this.numeric_green.Name = "numeric_green";
+            this.numeric_green.Size = new System.Drawing.Size(120, 23);
+            this.numeric_green.TabIndex = 1;
+            this.numeric_green.Maximum = 255;
+            // 
+            // numeric_red
+            // 
+            this.numeric_red.Location = new System.Drawing.Point(74, 20);
+            this.numeric_red.Name = "numeric_red";
+            this.numeric_red.Size = new System.Drawing.Size(120, 23);
+            this.numeric_red.TabIndex = 0;
+            this.numeric_red.Maximum = 255;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numeric_height);
+            this.groupBox2.Controls.Add(this.numeric_width);
+            this.groupBox2.Location = new System.Drawing.Point(219, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Size";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Width";
+            // 
+            // numeric_height
+            // 
+            this.numeric_height.Location = new System.Drawing.Point(74, 48);
+            this.numeric_height.Name = "numeric_height";
+            this.numeric_height.Size = new System.Drawing.Size(120, 23);
+            this.numeric_height.TabIndex = 1;
+            this.numeric_height.Minimum = 1;
+            this.numeric_height.Maximum = 100;
+            // 
+            // numeric_width
+            // 
+            this.numeric_width.Location = new System.Drawing.Point(74, 19);
+            this.numeric_width.Name = "numeric_width";
+            this.numeric_width.Size = new System.Drawing.Size(120, 23);
+            this.numeric_width.TabIndex = 0;
+            this.numeric_width.Minimum = 1;
+            this.numeric_width.Maximum = 100;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.numeric_posy);
+            this.groupBox3.Controls.Add(this.numeric_posx);
+            this.groupBox3.Location = new System.Drawing.Point(426, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Position";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(0, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 15);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 15);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "X";
+            // 
+            // numeric_posy
+            // 
+            this.numeric_posy.Location = new System.Drawing.Point(74, 51);
+            this.numeric_posy.Name = "numeric_posy";
+            this.numeric_posy.Size = new System.Drawing.Size(120, 23);
+            this.numeric_posy.TabIndex = 1;
+            this.numeric_posy.Maximum = 1000;
+            // 
+            // numeric_posx
+            // 
+            this.numeric_posx.Location = new System.Drawing.Point(74, 20);
+            this.numeric_posx.Name = "numeric_posx";
+            this.numeric_posx.Size = new System.Drawing.Size(120, 23);
+            this.numeric_posx.TabIndex = 0;
+            this.numeric_posx.Maximum = 1000;
+            // 
+            // button_add
+            // 
+            this.button_add.AutoSize = true;
+            this.button_add.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button_add.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button_add.Location = new System.Drawing.Point(652, 25);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(121, 30);
+            this.button_add.TabIndex = 3;
+            this.button_add.Text = "Add object";
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.AutoSize = true;
+            this.button_end.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button_end.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button_end.Location = new System.Drawing.Point(652, 81);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(87, 30);
+            this.button_end.TabIndex = 3;
+            this.button_end.Text = "End file";
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // Rectangle_generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 134);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Colors);
+            this.Name = "Rectangle_generator";
+            this.Text = "Rectangle svg generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Colors.ResumeLayout(false);
+            this.Colors.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_red)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_width)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_posy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_posx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Colors;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numeric_blue;
+        private System.Windows.Forms.NumericUpDown numeric_green;
+        private System.Windows.Forms.NumericUpDown numeric_red;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numeric_height;
+        private System.Windows.Forms.NumericUpDown numeric_width;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numeric_posy;
+        private System.Windows.Forms.NumericUpDown numeric_posx;
+        private System.Windows.Forms.Label button_add;
+        private System.Windows.Forms.Label button_end;
+    }
+}
+
---
+++ rectangle_svg_gen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace recrangle_svg_gen
+{
+    public partial class Rectangle_generator : Form
+    {
+        public Rectangle_generator()
+        {
+            InitializeComponent();
+        }
+
+        public void GetProperties()
+        {
+            properties = (numeric_width.Value, numeric_height.Value, numeric_posx.Value, numeric_posy.Value, (numeric_red.Value, numeric_green.Value, numeric_blue.Value));
+        }
+        
+        (decimal width, decimal height, decimal pos_x, decimal pos_y, (decimal red, decimal green, decimal blue) colors) properties;
+        StreamWriter sw = new StreamWriter("rectangles.svg");
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            GetProperties();
+            sw.WriteLine($"<rect x = \"{properties.pos_x}\" y = \"{properties.pos_y}\" width = \"{properties.width}\" height = \"{properties.height}\" style=\"fill: rgb{properties.colors}\" />");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sw.WriteLine("<svg width = \"1000\" height = \"1000\" version = \"1.1\" xmlns = \"http://www.w3.org/2000/svg\" >");
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            sw.Write("</svg>");
+            sw.Flush();
+            sw.Close();
+        }
+       
+
+    }
+}
